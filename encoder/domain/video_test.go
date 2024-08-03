85670034a8f67e1e3cd83729532ab86124d0a8b9
Giovane <giovaneaalmeida27@gmail.com>
2024-08-03 15:01:33 -0300

    feat: create video entity tests

---
+++ encoder/domain/video_test.go
@@ -0,0 +1,41 @@
+package domain_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/giovane-aG/video-encoder/encoder/domain"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIdIsNotUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Error(t, err)
+}
+
+func TestVideoIValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewString()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Nil(t, err)
+}
