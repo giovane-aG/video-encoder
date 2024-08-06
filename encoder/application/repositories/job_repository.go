9c837a10870b29583833ea4aec6bfcd26cbb142b
Giovane <giovaneaalmeida27@gmail.com>
2024-08-06 09:10:17 -0300

    feat: create find job function

--- encoder/application/repositories/job_repository.go
+++ encoder/application/repositories/job_repository.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"fmt"
+
 	"github.com/giovane-aG/video-encoder/encoder/domain"
 	"github.com/jinzhu/gorm"
 )
@@ -15,6 +17,17 @@ func (repo JobRepository) Insert(job *domain.Job) (*domain.Job, error) {
 
 	if err != nil {
 		return nil, err
+	}
+
+	return job, nil
+}
+
+func (repo JobRepository) Find(id string) (*domain.Job, error) {
+	var job *domain.Job
+	repo.DB.First(job, "id = ?", id)
+
+	if job.ID == "" {
+		return nil, fmt.Errorf("no job with this id was found")
 	}
 
 	return job, nil
