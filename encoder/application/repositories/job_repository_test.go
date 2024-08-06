7234c615f31bef07d994d3b9752d18582ff0180d
Giovane <giovaneaalmeida27@gmail.com>
2024-08-06 09:45:16 -0300

    test: insert job

---
+++ encoder/application/repositories/job_repository_test.go
@@ -0,0 +1,42 @@
+package repositories_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/giovane-aG/video-encoder/encoder/application/repositories"
+	"github.com/giovane-aG/video-encoder/encoder/domain"
+	"github.com/giovane-aG/video-encoder/encoder/infrastructure/database"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJobRepositoryDBInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	jobRepository := repositories.NewJobRepository(db)
+	videoRepository := repositories.NewVideoRepository(db)
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewString()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	videoRepository.Insert(video)
+
+	job, err := domain.NewJob(
+		"output",
+		"uploaded",
+		video,
+	)
+	require.Nil(t, err)
+
+	_, err = jobRepository.Insert(job)
+	require.Nil(t, err)
+
+	foundJob, err := jobRepository.Find(job.ID)
+	require.Nil(t, err)
+	require.Equal(t, foundJob.ID, job.ID)
+	require.Equal(t, job.VideoID, video.ID)
+}
