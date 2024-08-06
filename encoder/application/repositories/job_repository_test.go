97ae168f8487629fb561b90c114d2f85d72dae87
Giovane <giovaneaalmeida27@gmail.com>
2024-08-06 09:50:42 -0300

    test: update job

--- encoder/application/repositories/job_repository_test.go
+++ encoder/application/repositories/job_repository_test.go
@@ -40,3 +40,38 @@ func TestJobRepositoryDBInsert(t *testing.T) {
 	require.Equal(t, foundJob.ID, job.ID)
 	require.Equal(t, job.VideoID, video.ID)
 }
+
+func TestJobRepositoryDBUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	jobRepository := repositories.NewJobRepository(db)
+	videoRepository := repositories.NewVideoRepository(db)
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewString()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	videoRepository.Insert(video)
+
+	job, err := domain.NewJob(
+		"output",
+		"uploaded",
+		video,
+	)
+	require.Nil(t, err)
+
+	_, err = jobRepository.Insert(job)
+	require.Nil(t, err)
+
+	job.OutputBucketPath = "new_output_path"
+	_, err = jobRepository.Update(job)
+	require.Nil(t, err)
+
+	foundJob, err := jobRepository.Find(job.ID)
+	require.Nil(t, err)
+	require.Equal(t, foundJob.ID, job.ID)
+	require.Equal(t, foundJob.OutputBucketPath, "new_output_path")
+	require.Equal(t, job.VideoID, video.ID)
+}
