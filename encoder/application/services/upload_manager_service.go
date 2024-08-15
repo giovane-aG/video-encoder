c99349203e1f08a3b830fad14ab11ae12b7a7950
Giovane <giovaneaalmeida27@gmail.com>
2024-08-15 09:26:35 -0300

    feat: create upload video service

---
+++ encoder/application/services/upload_manager_service.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"context"
+	"io"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/storage"
+)
+
+type VideoUploadManagerService struct {
+	Paths        []string
+	VideoPath    string
+	OutputBucket string
+	Errors       []string
+}
+
+func NewVideoUploadManagerService() *VideoUploadManagerService {
+	return &VideoUploadManagerService{}
+}
+
+func (vu *VideoUploadManagerService) UploadObject(objectPath string, client *storage.Client, ctx context.Context) error {
+	path := strings.Split(objectPath, os.Getenv("localStoragePath"+"/"))
+
+	file, err := os.Open(objectPath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	wc := client.Bucket(vu.OutputBucket).Object(path[1]).NewWriter(ctx)
+	wc.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
+
+	if _, err = io.Copy(wc, file); err != nil {
+		return err
+	}
+
+	return nil
+}
