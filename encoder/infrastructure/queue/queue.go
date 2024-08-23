64172052ae504a9d1038e6cbba46ad0e5129f29c
Giovane <giovaneaalmeida27@gmail.com>
2024-08-23 09:19:26 -0300

    feat: create rabbitmq struct

---
+++ encoder/infrastructure/queue/queue.go
@@ -0,0 +1,38 @@
+package queue
+
+import (
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQ struct {
+	User              string
+	Password          string
+	Host              string
+	Port              string
+	Vhost             string
+	ConsumerQueueName string
+	ConsumerName      string
+	AutoAck           bool
+	Args              amqp.Table
+	Channel           *amqp.Channel
+}
+
+func NewRabbitMQ() *RabbitMQ {
+
+	rabbitMQArgs := amqp.Table{}
+	rabbitMQArgs["x-dead-letter-exchange"] = os.Getenv("RABBITMQ_DLX")
+
+	return &RabbitMQ{
+		User:              os.Getenv("RABBITMQ_DEFAULT_USER"),
+		Password:          os.Getenv("RABBITMQ_DEFAULT_PASS"),
+		Host:              os.Getenv("RABBITMQ_DEFAULT_HOST"),
+		Port:              os.Getenv("RABBITMQ_DEFAULT_PORT"),
+		Vhost:             os.Getenv("RABBITMQ_DEFAULT_VHOST"),
+		ConsumerQueueName: os.Getenv("RABBITMQ_CONSUMER_QUEUE_NAME"),
+		ConsumerName:      os.Getenv("RABBITMQ_CONSUMER_NAME"),
+		AutoAck:           false,
+		Args:              rabbitMQArgs,
+	}
+}
