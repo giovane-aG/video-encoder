a8655a1e6e739cac7a230ec985b1e0e7a7729866
Giovane <giovaneaalmeida27@gmail.com>
2024-08-27 10:37:58 -0300

    feat: create job worker

---
+++ encoder/application/services/job_woker.go
@@ -0,0 +1,82 @@
+package services
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+
+	"github.com/giovane-aG/video-encoder/encoder/domain"
+	"github.com/giovane-aG/video-encoder/encoder/infrastructure/utils"
+	"github.com/google/uuid"
+	"github.com/streadway/amqp"
+)
+
+type JobWorkerResult struct {
+	Job     domain.Job
+	Message *amqp.Delivery
+	Error   error
+}
+
+func JobWorker(
+	messageChannel chan amqp.Delivery,
+	returnChan chan JobWorkerResult,
+	jobService JobService,
+	job domain.Job,
+	workerID int,
+) {
+	for message := range messageChannel {
+
+		if err := utils.IsJson(string(message.Body)); err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err := json.Unmarshal(message.Body, &jobService.VideoService.Video)
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.Video.Validate()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.InsertVideo()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		job.Video = jobService.VideoService.Video
+		job.OutputBucketPath = os.Getenv("outputBucketPath")
+		job.ID = uuid.NewString()
+		job.Status = "STARTING"
+		job.CreatedAt = time.Now()
+
+		_, err = jobService.JobRepository.Insert(&job)
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		jobService.Job = &job
+
+		err = jobService.Start()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		returnChan <- returnJobResult(job, message, nil)
+	}
+}
+
+func returnJobResult(job domain.Job, message amqp.Delivery, err error) JobWorkerResult {
+	return JobWorkerResult{
+		Job:     job,
+		Message: &message,
+		Error:   err,
+	}
+}
