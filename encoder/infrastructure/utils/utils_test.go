8ed61bee9e2dcaaaab19a8ddd18b1ecaab1b8689
Giovane <giovaneaalmeida27@gmail.com>
2024-08-27 09:12:23 -0300

    test: should not return error when a valid json is used

--- encoder/infrastructure/utils/utils_test.go
+++ encoder/infrastructure/utils/utils_test.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/giovane-aG/video-encoder/encoder/infrastructure/utils"
@@ -12,5 +13,21 @@ func TestShouldReturnErrorWhenJsonIsNotValid(t *testing.T) {
 
 	err := utils.IsJson(invalidJson)
 
-	require.NotNil(t, err)
+	require.Error(t, err)
+}
+
+func TestShouldReturnNilIfJsonIsValid(t *testing.T) {
+	st := struct {
+		Id int
+	}{
+		Id: 1234,
+	}
+
+	validJson, err := json.Marshal(&st)
+
+	require.Nil(t, err)
+
+	err = utils.IsJson(string(validJson))
+	require.Nil(t, err)
+
 }
